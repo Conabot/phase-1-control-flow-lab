9db3d3b17bbb81e021f45c9bb5a4e904fe082ed9
Minh Tran <tdaimike@gmail.com>
2023-03-16 17:16:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let result;
+ if (distance < 400 ) {
+    result = 'This one is on me!';
+  } else if (distance < 2000) {
+    result = "That will be twenty bucks.";
+  } else if   (distance > 2000){
+    result = "I will gladly take your thirty bucks.";
+    if (distance > 2500) {
+      result = "No can do."
+    } 
+  }
+ return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  let checkCity; 
   // Write your code here!
+  return (city === 'NYC' ? "Ok, sounds good." : "No go."); 
+   
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  let says;
+  switch (tip) {
+    case 'generous' :
+      says = "Thank you so much.";
+    break; 
+    case 'not as generous':
+      says = "Thank you.";
+    break;
+   default:
+      says = "Bye."
+    break;
+  }
+  return says;
 }
